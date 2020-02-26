7b43ef8ff9c354dcbcf6ca66ed97b42f8eade7f9
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-02-26 08:23:53 +0200

    some bindings

--- hello-world/src/app/courses.component.ts
+++ hello-world/src/app/courses.component.ts
@@ -12,15 +12,42 @@ import { CourseService } from './courses.service';
         <ul>
             <li *ngFor="let course of courses">{{course}}</li>
         <ul>
-    `
+        <img [src]="imgUrl"/>
+        <table>
+            <tr>
+                <td [attr.colspan]="colSpan"></td>
+            </tr>
+        </table>
+        <button class="btn btn-primary" (click)="onSave($event)">Push Me!</button>
+        <input (keyUp.enter)="onKeyUp()"/>
+        <input (keyUp.enter)="onKeyUpWithVar()"/>
+        `
 })
 export class CoursesComponent
 {
     title='List of courses.';
     courses;
+    imgUrl='https://live.staticflickr.com/4112/5170590074_714d36db83_b.jpg';
+    colSpan=2;
 
     constructor(service:CourseService)
     {
         this.courses=service.getCourses();
+    }
+
+    onSave($event)
+    {
+        $event.stopPrpagation;
+        console.log('Button was clicked');
+    }
+
+    onKeyUp()
+    {
+        console.log('Enter was pressed');
+    }
+
+    onKeyUpWithVar(email)
+    {
+        console.log(email);
     }
 }
